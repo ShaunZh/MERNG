9024f0e43a878e63446cfb46e638cce9660bdbd2
ShaunZh <308826842@qq.com>
2024-03-16 00:28:56 +0800

    update: resolvers

---
+++ graphql/resolvers/index.js
@@ -0,0 +1,12 @@
+const postsResolvers = require('./posts.js')
+const usersResolvers = require('./users.js')
+
+module.exports = {
+    Query: {
+        ...postsResolvers.Query,
+        ...usersResolvers.Query
+    },
+    Mutation: {
+        ...usersResolvers.Mutation
+    }
+}
