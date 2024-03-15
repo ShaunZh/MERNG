81f85ab6c96cb95dfc9c3daa9ef4cebfc412ade5
ShaunZh <308826842@qq.com>
2024-03-16 00:29:08 +0800

    update: typeDefs

---
+++ graphql/typeDefs.js
@@ -0,0 +1,31 @@
+const { gql } = require('apollo-server');
+
+module.exports = gql`
+    type Post {
+        id: ID!,
+        post: String!,
+        username: String!,
+        createdAt: String!,
+    }
+    type Query {
+        getPosts: [Post]!
+    }
+
+    type User {
+        id: ID!,
+        token: String!
+        username: String!
+        email: String!
+        createdAt: String!
+    }
+
+    input RegisterInput {
+        username: String!,
+        password: String!,
+        confirmPassword: String!,
+        email: String!
+    }
+    type Mutation {
+        register(registerInput: RegisterInput!): User!
+    }
+`
