68b2382c85361870812a9ba178be861837bb212f
ShaunZh <308826842@qq.com>
2024-03-16 00:29:15 +0800

    add: validators

---
+++ util/validators.js
@@ -0,0 +1,29 @@
+module.exports.validateRegisterInput = (
+    username,
+    pasword,
+    confirmPassword,
+    email
+) => {
+    const errors = {}
+    if (username.trim() === '') {
+        errors.username = 'Username must not be empty'
+    }
+    if (email.trim() === '') {
+        errors.email = 'Email must not be empty'
+    } else {
+        const regEx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+        if (!email.match(regEx)) {
+            errors.email = 'Email must be a valid email address'
+        }
+    }
+    if (pasword === '') {
+        errors.password = 'Password must not be empty'
+    } else if (pasword !== confirmPassword) {
+        errors.confirmPassword = 'Passwords must match'
+    }
+
+    return {
+        errors,
+        valid: Object.keys(errors).length < 1
+    }
+}
