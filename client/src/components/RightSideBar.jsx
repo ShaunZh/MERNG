3f85a42b792a6e352bae9d55a31454989741a45b
ShaunZh <308826842@qq.com>
2024-03-20 11:01:23 +0800

    update: LeftSideBar style

--- client/src/components/RightSideBar.jsx
+++ client/src/components/RightSideBar.jsx
@@ -2,10 +2,16 @@
 function LeftSideBar() {
     return (
         <div className='w-3/4 pl-8 pt-4 h-full'>
-            <div className='flex py-3 px-4 bg-slate-100 rounded-full overflow-hidden items-center text-slate-500 text-lg'>
+            <div className='flex py-3 px-4 bg-slate-100 rounded-full overflow-hidden items-center text-slate-500 text-lg 
+            focus:border-sky-500 focus:border
+            hover:border hover:border-sky-500
+            border border-transparent
+            transition duration-100 hover:ease-in
+            focus:text-sky-500 hover:text-sky-500
+            '>
                 <i aria-hidden="true" className="search icon pb-6" />
                 <input type="text" placeholder="Search"
-                    className='w-full  bg-slate-100 h-8 border-0 focus:outline-none' />
+                    className='w-full  bg-slate-100 h-8 focus:outline-none text-slate-800' />
             </div>
         </div>
     )
