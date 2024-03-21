84d86f2809fa20c94d1a1df49361f9a6f46ceaf7
ShaunZh <308826842@qq.com>
2024-03-21 17:28:02 +0800

    client: update sidebar

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,8 +1,30 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate} from "react-router-dom";
+import { useState } from "react";
+import { Dropdown } from 'antd'
+import { EllipsisOutlined  } from '@ant-design/icons';
+
+import CreatePost from './CreatePost'
+import { TOKEN_KEY } from "../utils/constants";
+const items = [
+    {
+        key: 'logout',
+        label: <span className="cursor-pointer w-10">logout</span>,
+    },
+];
+
 
 function SideBar() {
+    const [visibleCreatePost, setVisibleCreatePost] = useState(false)
+    const navigate = useNavigate();
+
+    const handleMenuClick = (e) => {
+        if (e.key === 'logout') {
+            sessionStorage.removeItem(TOKEN_KEY)
+            navigate('/login')
+        }
+    }
     return (
-        <div className='flex flex-col justify-between w-2/3 mb-4'>
+        <div className='flex flex-col justify-between mb-4 '>
             <div className="top flex flex-col flex-1 ">
                 <Link to='/home' className='py-4  px-8 text-4xl cursor-pointer'>
                     <i className="bullseye icon "></i>
@@ -11,15 +33,27 @@ function SideBar() {
                 <Link to='/explore' className='py-4 hover:bg-slate-100 rounded-full px-8 text-2xl'>Explore</Link>
                 <Link to='/notifications' className='py-4 hover:bg-slate-100 px-8 rounded-full text-2xl'>Notifications</Link>
                 <Link className='py-4 hover:bg-slate-100 rounded-full px-8 text-2xl mb-4'>Profile</Link>
-                <button className='py-4 hover:bg-sky-300 bg-sky-400 rounded-full  text-2xl text-white duration-300'>Post</button>
+                <button type="primary" className='py-4 hover:bg-sky-300 bg-sky-400 rounded-full  text-2xl text-white duration-300' onClick={() => setVisibleCreatePost(true)}>Post</button>
             </div>
-            <div className='flex hover:bg-slate-100 rounded-full px-8 py-2 cursor-pointer'>
+            <div className='flex hover:bg-slate-100 rounded-full px-2 py-2 cursor-pointer items-center justify-between'>
                 <span className='size-10 rounded-full bg-sky-500 text-center leading-10 text-white mr-4' >U</span>
                 <div className='flex flex-col'>
-                    <span className="text-xl font-bold">username</span>
+                    <span className=" font-bold">username</span>
                     <span>@username</span>
                 </div>
+                <Dropdown
+                    menu={{
+                        items,
+                        onClick: handleMenuClick
+                    }}
+                >
+                    <EllipsisOutlined className="font-medium text-2xl"/>
+                </Dropdown>
             </div>
+            <CreatePost
+                visible={visibleCreatePost}
+                handleClose={() => setVisibleCreatePost(false)}
+            />
         </div>
     )
 }
