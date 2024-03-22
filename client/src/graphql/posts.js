6319de33e6a9227c3531a259a11a6cdeda1b970e
ShaunZh <308826842@qq.com>
2024-03-22 14:47:17 +0800

    add: subscribe create post

--- client/src/graphql/posts.js
+++ client/src/graphql/posts.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 export const QUERY_POSTS = gql`
-    query ExampleQuery {
+    query GetPosts {
         getPosts {
             id
             post
@@ -19,4 +19,37 @@ export const CREATE_POST = gql`
         post
       }
     }
+`
+
+// export const POSTS_SUBSCRIPTION = gql`
+//   subscription POSTS_SUBSCRIPTION() {
+//     POST_CREATED() {
+//       id
+//       content
+//     }
+//   }
+// `;
+
+export const NEW_SUBSCRIPTION = gql`
+subscription Subscription {
+  postCreated {
+    id
+    post
+    username
+    createdAt
+    comments {
+      id
+      body
+      username
+      createdAt
+    }
+    likes {
+      id
+      username
+      createdAt
+    }
+    likeCount
+    commentCount
+  }
+}
 `
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { message } from 'antd'
 import ReactDOM from 'react-dom/client'
-import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, from } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, from, split } from '@apollo/client';
 import { onError } from "@apollo/client/link/error";
 import { StyleProvider } from '@ant-design/cssinjs';
 import { ErrorBoundary } from 'react-error-boundary'
+import { getMainDefinition } from '@apollo/client/utilities';
+
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { createClient } from 'graphql-ws';
+
 import './index.css'
 import './styles/index.css'
 import App from './App'
@@ -13,8 +18,14 @@ import { TOKEN_KEY } from './utils/constants';
 const errorLink = onError((error) => {
   const { graphQLErrors, networkError } = error;
   console.log('error', error);
-  if (graphQLErrors)
+  if (graphQLErrors) {
     message.error(graphQLErrors[0].message)
+    if (graphQLErrors[0].extensions.code === 'UNAUTHENTICATED') {
+      sessionStorage.removeItem(TOKEN_KEY)
+      window.location.href = '/login'
+    }
+  }
+  
   if (networkError) {
     message.error(networkError)
   }
@@ -27,11 +38,33 @@ const httpLink = new HttpLink({
   }
 })
 
+const wsLink = new GraphQLWsLink(createClient({
+  url: 'ws://localhost:5000/graphql',
+  headers: {
+    authorization: `Bearer ${sessionStorage.getItem(TOKEN_KEY)}`
+  }
+  
+}));
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    console.log('definition', definition);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
+
+
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: from([errorLink, httpLink]),
- 
+  link: from([errorLink, splitLink]),
 });
+
 
 // Supported in React 18+
 const root = ReactDOM.createRoot(document.getElementById('root'));
